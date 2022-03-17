b2b20fd06d78e2249b151f30169f7c8dc68f8bd3
KB731 <kevinbette.formapro@gmail.com>
2022-03-17 15:11:29 +0100

    Création Database

--- Database/createDB.php
+++ Database/createDB.php
@@ -2,7 +2,7 @@
 
 require_once 'connectDB.php';
 
-$create_database = "CREATE DATABASE projet_ampoule";
+$create_database = "CREATE DATABASE `projet_ampoule`";
    
 $connectDB->exec($create_database);
 echo 'Database created !';
--- Database/createTable.php
+++ Database/createTable.php
@@ -1,12 +1,13 @@
 <?php
+
 require_once 'connectDB.php';
 
-$create_table = "CREATE TABLE ampoule(
-    Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    Date DATE NOT NULL,
-    Etage INT(2) NOT NULL,
-    Emplacement VARCHAR(255) NOT NULL,
-    Prix DECIMAL(6,2) NOT NULL,
+$create_table = "CREATE TABLE IF NOT EXISTS `ampoule`(
+    `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `date` DATE  NOT NULL,
+    `etage` INT(2) NOT NULL,
+    `emplacement` VARCHAR(255) NOT NULL,
+    `prix` DECIMAL(6,2) NOT NULL
     )";
 
 $connectDB->exec($create_table);
